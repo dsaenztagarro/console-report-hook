7504a1072c65ffc5c128dd78fa09a921dcb8a981
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-08-09 02:04:32 +0200

    Fixed message builder

--- src/main/java/com/liferay/challenge/util/MessageBuilder.java
+++ src/main/java/com/liferay/challenge/util/MessageBuilder.java
@@ -1,14 +1,22 @@
+
 package com.liferay.challenge.util;
 
 import java.util.ArrayList;
 
 public class MessageBuilder {
-		
-	public static String build(String organizationName, ArrayList<Integer>divisibleValues) {
+
+	public static String build(
+		String organizationName, ArrayList<Integer> divisibleValues) {
+
+		if (organizationName == null || organizationName.equals("") ||
+			divisibleValues == null) {
+			return "";
+		}
 		int size = divisibleValues.size();
 		StringBuffer messageBuffer = new StringBuffer(organizationName);
 		if (size > 0) {
-			messageBuffer.append(", divisible en grupos de ").append(divisibleValues.remove(0));
+			messageBuffer.append(", divisible en grupos de ").append(
+				divisibleValues.remove(0));
 			size--;
 			for (int i = 0; i < size; i++) {
 				messageBuffer.append(" o de ").append(divisibleValues.get(i));
@@ -16,5 +24,5 @@ public class MessageBuilder {
 		}
 		return messageBuffer.toString();
 	}
-	
+
 }
--- src/test/java/com/liferay/challenge/util/MessageBuilderTest.java
+++ src/test/java/com/liferay/challenge/util/MessageBuilderTest.java
@@ -1,8 +1,11 @@
+
 package com.liferay.challenge.util;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -10,38 +13,50 @@ public class MessageBuilderTest {
 
 	@Test
 	public void testNoDivisibleValues() {
+
 		String organizationName = "Liferay Inc.";
 
 		ArrayList<Integer> divisibleValues = new ArrayList<Integer>();
 
-		String message = MessageBuilder.build(organizationName, divisibleValues);
-		
+		String message =
+			MessageBuilder.build(organizationName, divisibleValues);
+
 		assertTrue(message.equals(organizationName));
 	}
 
 	@Test
 	public void testOneDivisibleValues() {
+
 		String organizationName = "Liferay Inc.";
 
 		ArrayList<Integer> divisibleValues = new ArrayList<Integer>();
 		divisibleValues.add(3);
 
-		String message = MessageBuilder.build(organizationName, divisibleValues);
-		
+		String message =
+			MessageBuilder.build(organizationName, divisibleValues);
+
 		assertTrue(message.equals("Liferay Inc., divisible en grupos de 3"));
 	}
 
 	@Test
 	public void testMoreThanOneDivisibleValues() {
+
 		String organizationName = "Liferay Inc.";
 
 		ArrayList<Integer> divisibleValues = new ArrayList<Integer>();
 		divisibleValues.add(3);
 		divisibleValues.add(5);
 
-		String message = MessageBuilder.build(organizationName, divisibleValues);
-		
+		String message =
+			MessageBuilder.build(organizationName, divisibleValues);
+
 		assertTrue(message.equals("Liferay Inc., divisible en grupos de 3 o de 5"));
 	}
-	
+
+	@Test
+	public void testInvalidArguments() {
+
+		String message = MessageBuilder.build(null, null);
+		assertTrue(message.equals(""));
+	}
 }
